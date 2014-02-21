9a8f47886100b72cd8547119d24ea08b35b8181f
6174 <570171025@qq.com>
2014-02-21 16:55:33 +0800

    继续完善parser, 做对雨函数声明以及函数表达式的spy

---
+++ doc/spike-spider-monkey-parser-api.md
@@ -0,0 +1,18 @@
+##esprima parser api
+
+###基本Node类型
+* Declaration：
+    - FunctionDeclaration: 函数声明
+    - VariableDeclaration: 变量声明
+* Expression
+    - ThisExpression
+    - ArrayExpression
+    - ArrayExpression
+    - ObjectExpression
+    - FunctionExpression
+    - AssignmentExpression
+    - UpdateExpression  
+    - LogicalExpression 
+    - ConditionalExpression 
+    - NewExpression 
+    - CallExpression 
--- src/proxy/parser.js
+++ src/proxy/parser.js
@@ -17,72 +17,166 @@
          spy('MyClass.doAdd'),
          spy('helloWorld.doAdd')
  *   
+ * 在ast里边目前有两种情况：
+ *  1. FunctionDeclaration: 
+ 				eg：
+ 				 function add(){}
+ 		   这个时候的处理应该为在其后面添加一个spystatement
+ 		   编译过后应该为
+ 		     function add(){}
+ 		     spy(add, '*.add');
+ 		2. FunctionExpression, 匿名函数可以在任何表达式上面出现， 要把这个值替换
+ 		  为spy(function(){}, '**.$f*');
  */
 
 //--requires
 var _ = require('underscore');
+var util = require('util');
 var esprima = require('esprima');
 var escodegen = require('escodegen');
+var estraverse = require('estraverse');
 
 //--users
 var anonymouseScopeCounter = 0;
 var fileName;
+
+//--每个模块的， 可以设定为fileName
+var moduleName = '';
 var options = {
 	insSpyName: 'spy'
 };
 
 
 function injectInstrument(syntax) {
-	traverse(syntax, traverser);
+	console.log('***************************before\n');
+	console.log(util.inspect(syntax.body, false, 10));
+	console.log(escodegen.generate(syntax));
+
+
+	traverse(syntax, traverser, {
+		blockName: moduleName,
+		parent: null,
+		key: null
+	});
+	
+
+
+
+	console.log('***************************after\n');
+	console.log(util.inspect(syntax.body, false, 10));
+	// console.log(escodegen.generate(syntax));
 	return syntax;
 }
 
-function traverser(node) {
-	if (node.type === 'ConditionalExpression') {
-		checkConditional(node);
-	}
-}
-
-function checkConditional(node) {
-	var condition;
-
-	if (node.consequent.type === 'ConditionalExpression' ||
-		node.alternate.type === 'ConditionalExpression') {
-
-		condition = content.substring(node.test.range[0], node.test.range[1]);
-		if (condition.length > 20) {
-			condition = condition.substring(0, 20) + '...';
-		}
-		condition = '"' + condition + '"';
-		report(node, 'Nested ternary for ' + condition);
-	}
-}
-
 // Executes visitor on the object and its children (recursively).
-function traverse(object, visitor) {
+function traverse(object, visitor, attach) {
 	var key, child;
 
-	visitor.call(null, object);
+	var attach = visitor.call(null, object, attach);
+	attach.parent = object;
+
 	for (key in object) {
 		if (object.hasOwnProperty(key)) {
 			child = object[key];
 			if (typeof child === 'object' && child !== null) {
-				traverse(child, visitor);
+				attach.key = key;
+				traverse(child, visitor, attach);
 			}
 		}
 	}
 }
 
-function getAnonymouseScopeName() {
-	return '$f' + anonymouseScopeCounter++;
+function traverser(node, attach) {
+	var type =  node.type;
+	var ret = attach;
+	switch (true) {
+		case type === 'FunctionExpression':
+			ret = handleFunctionExpression(node, attach);
+			console.log('blockName: ' + ret && ret.blockName);
+			break;
+		case type === 'FunctionDeclaration':
+			ret = handleFunctionDeclaration(node, attach);
+			console.log('blockName: ' + ret && ret.blockName);
+			break;
+		default:
+			break;
+	}
+	return ret;
 }
 
 
+function handleFunctionExpression(node, attach) {
+	//--每次traverse过后添加函数的名称全局域名
+	var parantBlockName = attach.blockName ? attach.blockName + '.' : '';
+	if (node.id === null) {
+		node.id = getAnonymouseScopeId();
+	}
+	attach.parent[attach.key] = getSpyExpression(node);
+	return {
+		blockName: parantBlockName + node.id.name
+	}
+}
+
+
+function handleFunctionDeclaration(node, attach) {
+	var parentBlockName = attach.blockName ? attach.blockName + '.' : '';
+	return {
+		blockName: parentBlockName + node.id.name
+	}
+}
+
+
+function getAnonymouseScopeId() {
+	return {
+		"type": "Identifier",
+		"name": '$f' + anonymouseScopeCounter++
+	}
+}
+
+function getSpyExpressionStatement(id) {
+	return {
+		"type": "ExpressionStatement",
+		"expression": {
+			"type": "CallExpression",
+			"callee": {
+				"type": "Identifier",
+				"name": "spy"
+			},
+			"arguments": [{
+				"type": "Identifier",
+				"name": "a"
+			}, {
+				"type": "Literal",
+				"value": id,
+				"raw": "'" + id + "'"
+			}]
+		}
+	}
+}
+
+function getSpyExpression(functionExpression, id) {
+	return {
+		"type": "CallExpression",
+		"callee": {
+			"type": "Identifier",
+			"name": "spy"
+		},
+		"arguments": [
+			functionExpression, {
+				"type": "Literal",
+				"value": id,
+				"raw": "'" + id + "'"
+			}
+		]
+	}
+}
+
 function test() {
-	var syntax = esprima.parse("var name = 'hello world';", {
+	var code = "var a = function (b){ return function c(){}; } ";
+	var syntax = esprima.parse(code, {
 		tolerant: true,
-		loc: true,
-		range: true
+		loc: false,
+		range: false
 	});
 	return injectInstrument(syntax);
 }
